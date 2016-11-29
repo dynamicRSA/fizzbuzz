852c685cba042ac7bad54b6942f4f0d199d2a3f3
Sven Prinsloo <sven@LAWTrust.co.za>
2016-11-29 09:41:44 +0200

    Initial fizzbuzz commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace FizzBuzzFormsApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buzzValue = new System.Windows.Forms.TextBox();
+            this.fizzValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buzzValue
+            // 
+            this.buzzValue.Location = new System.Drawing.Point(139, 34);
+            this.buzzValue.Name = "buzzValue";
+            this.buzzValue.Size = new System.Drawing.Size(100, 20);
+            this.buzzValue.TabIndex = 0;
+            // 
+            // fizzValue
+            // 
+            this.fizzValue.Location = new System.Drawing.Point(15, 34);
+            this.fizzValue.Name = "fizzValue";
+            this.fizzValue.Size = new System.Drawing.Size(100, 20);
+            this.fizzValue.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Fizz Value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Buzz Value";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(224, 238);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 370);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fizzValue);
+            this.Controls.Add(this.buzzValue);
+            this.Name = "Form1";
+            this.Text = "Fizz Buzz";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox buzzValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.TextBox fizzValue;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBuzzFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        //int fizzValue =0;
+        //int buzzValue =0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //fizz buzz logic 
+                for (int i = 1; i <= 100; i++)
+                {
+                    if (i % (Convert.ToInt32(fizzValue.Text)) == 0)
+                    {
+                        //out fizz
+                        Console.WriteLine("Fizz");
+                        listBox1.Items.Add("Fizz");
+                    }
+                    else if (i % (Convert.ToInt32(buzzValue.Text)) == 0)
+                    {
+                        //out buzz
+                        Console.WriteLine("Buzz");
+                        listBox1.Items.Add("Buzz");
+                    }
+                    else if ((i % (Convert.ToInt32(fizzValue.Text)) == 0) && (i % (Convert.ToInt32(buzzValue.Text)) == 0))
+                    {
+                        //out fizzbuzz
+                        Console.WriteLine("FizzBuzz");
+                        listBox1.Items.Add("FizzBuzz");
+                    }
+                    else {
+                        Console.WriteLine(i);
+                        listBox1.Items.Add(i);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to run logic " + ex);
+            }
+        }
+    }
+}
